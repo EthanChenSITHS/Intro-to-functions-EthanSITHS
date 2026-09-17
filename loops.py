0974f6ff3af34351ad5076201d51f5153184418c
EthanChenSITHS <ethanc467@nycstudents.net>
2026-09-17 09:19:29 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -4,9 +4,38 @@ t = Turtle()
 
 t.shape('turtle')
 
-def square(): 
-    for i in range(60):
-        t.forward(100)
-        t.left(5)
+def square(x,turn): 
+    for i in range(4):
+        # i is an incrementor/iterator
+        t.forward(x)
+        t.left(turn)
+square(200,90)
 
-square()
+def triangle(x,turn): 
+    for i in range(3):
+        t.forward(x)
+        t.left(turn)
+triangle(200,120)
+
+# def squaresixty():
+#     t.speed(5000)
+#     for i in range(60):
+#         square(200,90)
+#         t.right(5)
+# squaresixty()
+
+# def trianglesixty():
+#     t.speed(5000)
+#     for i in range(60):
+#         triangle(200,120)
+#         t.right(5)
+# trianglesixty()
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(sidelength,rotate)
+
