3dbadfda1a4a1863a8973b6682de439017ca55f3
EthanChenSITHS <ethanc467@nycstudents.net>
2026-09-17 08:40:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,72 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
-turtle.done() 
+# """ t.forward(200) """
+
+# def message(input):
+#     print(input)
+# message("Hello Class")
+
+# """ def square(x):
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+
+# square(200) """
+
+
+# """ def equal(x):
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+#     t.left(120)
+#     t.forward(x)
+# equal(200)  """
+
+
+# def right():
+#     t.forward(100)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(135)
+#     t.forward(142)
+# right()
+# The right triangle does not need an input, you can do it by manually assigning the values. Note that the first triangle is commented so it will not run.
+
+def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+rectangle() 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
